229762deafc5fc30bdc875d9da31f216a301b37e
PureumJeon <jpurm0724@gmail.com>
2020-10-02 17:15:31 +0900

    Updates

---
+++ precache-manifest.ad30751542b872e568f1e20b69418d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f5aa5f084104546fe4af01eaaf1503",
+    "url": "/animal_age_calculator/index.html"
+  },
+  {
+    "revision": "c7564f95124878483f0e",
+    "url": "/animal_age_calculator/static/css/main.bbec99b8.chunk.css"
+  },
+  {
+    "revision": "86c4c56a8fca5fff338f",
+    "url": "/animal_age_calculator/static/js/2.a9d8e64b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animal_age_calculator/static/js/2.a9d8e64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7564f95124878483f0e",
+    "url": "/animal_age_calculator/static/js/main.cdf6d8fb.chunk.js"
+  },
+  {
+    "revision": "f2330cec981ab8a2dd8e",
+    "url": "/animal_age_calculator/static/js/runtime-main.8e17397c.js"
+  }
+]);
